1bd28517a6b857e75ee2022d54577d290317c03b
Justin Collins <justcollins@comcast.net>
2016-02-24 11:32:42 -0800

    health scripts and camera changes
    Created health scripts for the player and enemies using a base class for
    the health and subclasses for player and enemies. The camera to player
    script was also changed to include more options for where the camera can
    be changed and rotation to it, along with scrolling when the mouse is at
    the edge.

--- HotSpaceLine/Assets/Scripts/CollinsJustin/Camera/CameraToPlayer.cs
+++ HotSpaceLine/Assets/Scripts/CollinsJustin/Camera/CameraToPlayer.cs
@@ -8,24 +8,77 @@ Your player character must have the tag Player.
 
 public class CameraToPlayer : MonoBehaviour {
 
-    public float cameraDistance = 10.0f;
+    public float cameraDistanceX = 0.0f;
+    public float cameraDistanceY = 10.0f;
+    public float cameraDistanceZ = 10.0f;
+    public float cameraRotationX = 0.0f;
+    public float cameraRotationY = 0.0f;
+    public float cameraRotationZ = 0.0f;
+    public int screenBoundary = 50;
+    public float scrollSpeed = 5;
 
     private GameObject player;
+    private float currentCamDisX;
+    private float currentCamDisY;
+    private float currentCamDisZ;
+    private float camMouseMoveX;
+    private float camMouseMoveY;
+    private int screenWidth;
+    private int screenHeight;
     private Vector3 applyCameraDistance;
+    private Vector3 applyCameraRotation;
 
 	void Awake () {
-        applyCameraDistance = new Vector3(0.0f, cameraDistance, 0.0f);
-        this.gameObject.transform.rotation = Quaternion.Euler(90.0f, 0.0f, 0.0f);
-        this.gameObject.transform.position += applyCameraDistance;
         player = GameObject.FindGameObjectWithTag("Player");
+        currentCamDisX = cameraDistanceX;
+        currentCamDisY = cameraDistanceY;
+        currentCamDisZ = cameraDistanceZ;
 	}
+
+    void Start() {
+        applyCameraDistance = new Vector3(player.transform.position.x + currentCamDisX, player.transform.position.y + currentCamDisY, player.transform.position.z - currentCamDisZ);
+        applyCameraRotation = new Vector3(cameraRotationX, cameraRotationY, cameraRotationZ);
+        this.gameObject.transform.rotation = Quaternion.Euler(applyCameraRotation);
+        this.gameObject.transform.position += applyCameraDistance;
+        screenWidth = Screen.width;
+        screenHeight = Screen.height;
+    }
 	
 	void Update () {
-        TrackPlayer();
+        if(Input.GetKey(KeyCode.Space)) {
+            MouseScreenMovement();
+        } else {
+            camMouseMoveX = 0;
+            camMouseMoveY = 0;
+            TrackPlayer();
+        }
     }
 
     private void TrackPlayer() {
-        applyCameraDistance.Set(player.transform.position.x, cameraDistance, player.transform.position.z);
+        applyCameraDistance.Set(player.transform.position.x + currentCamDisX, player.transform.position.y + currentCamDisY, player.transform.position.z - currentCamDisZ);
+        this.gameObject.transform.position = applyCameraDistance;
+    }
+
+    private void MouseScreenMovement() {
+        camMouseMoveX = 0.0f;
+        camMouseMoveY = 0.0f;
+        if (Input.mousePosition.x > screenWidth - screenBoundary) {
+            camMouseMoveX += scrollSpeed * Time.deltaTime;
+            //this.gameObject.transform.position.x += scrollSpeed * Time.deltaTime;
+        } else if (Input.mousePosition.x < 0 + screenBoundary) {
+            camMouseMoveX -= scrollSpeed * Time.deltaTime;
+            //transform.position.x -= scrollSpeed * Time.deltaTime;
+        }
+
+        if (Input.mousePosition.y > screenHeight - screenBoundary) {
+            camMouseMoveY += scrollSpeed * Time.deltaTime;
+            //transform.position.z += scrollSpeed * Time.deltaTime;
+        } else if (Input.mousePosition.y < 0 + screenBoundary) {
+            camMouseMoveY -= scrollSpeed * Time.deltaTime;
+            //transform.position.z -= scrollSpeed * Time.deltaTime;
+        }
+
+        applyCameraDistance.Set(this.gameObject.transform.position.x + camMouseMoveX, this.gameObject.transform.position.y, this.gameObject.transform.position.z + camMouseMoveY);
         this.gameObject.transform.position = applyCameraDistance;
     }
 }
