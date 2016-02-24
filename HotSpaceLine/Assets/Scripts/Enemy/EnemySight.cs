136c4e61364c40b8cc6b954bcefa67fd7bcafc24
Chinchilla03 <Chinchilla03@gmail.com>
2016-02-24 11:37:47 -0800

    AI almost perfect
    need to add collider for room detection.

--- HotSpaceLine/Assets/Scripts/Enemy/EnemySight.cs
+++ HotSpaceLine/Assets/Scripts/Enemy/EnemySight.cs
@@ -8,99 +8,71 @@ public class EnemySight : MonoBehaviour
     public Vector3 personalLastSighting;            // Last place this enemy spotted the player.
 
 
-    private NavMeshAgent nav;                       // Reference to the NavMeshAgent component.
+    //private NavMeshAgent nav;                       // Reference to the NavMeshAgent component.
     private SphereCollider col;                     // Reference to the sphere collider trigger component.
-    private Animator anim;                          // Reference to the Animator.
-    //private LastPlayerSighting lastPlayerSighting;  // Reference to last local sighting of the player.
+    //private Animator anim;                          // Reference to the Animator.
     private GameObject player;                      // Reference to the player.
-    private Animator playerAnim;                    // Reference to the player's animator component.
+    //private Animator playerAnim;                    // Reference to the player's animator component.
     private int playerHealth;              // Reference to the player's health script.
-    //private HashIDs hash;                           // Reference to the HashIDs.
-    //private Vector3 previousSighting;               // Where the player was sighted last frame.
+    public Transform dumb;
 
 
     void Awake()
     {
         // Setting up the references.
-        nav = GetComponent<NavMeshAgent>();
+        //nav = GetComponent<NavMeshAgent>();
         col = GetComponent<SphereCollider>();
-        anim = GetComponent<Animator>();
-        //lastPlayerSighting = GameObject.FindGameObjectWithTag(Tags.gameController).GetComponent<LastPlayerSighting>();
-        player = GameObject.FindGameObjectWithTag("player");
-        //playerAnim = player.GetComponent<Animator>();
-        //playerHealth = player.GetComponent<PlayerHealth>();
-        //hash = GameObject.FindGameObjectWithTag(Tags.gameController).GetComponent<HashIDs>();
-
-        // Set the personal sighting and the previous sighting to the reset position.
-        //personalLastSighting = lastPlayerSighting.resetPosition;
-        //previousSighting = lastPlayerSighting.resetPosition;
+        //anim = GetComponent<Animator>();
+        player = GameObject.FindGameObjectWithTag("Player");
     }
 
 
     void Update()
     {
-        // If the last global sighting of the player has changed...
-        //if (lastPlayerSighting.position != previousSighting)
-            // ... then update the personal sighting to be the same as the global sighting.
-            //personalLastSighting = lastPlayerSighting.position;
-
-        // Set the previous sighting to the be the sighting from this frame.
         //previousSighting = personalLastSighting;
-
-        // If the player is alive...
-        //if (playerHealth.health > 0f)
-            // ... set the animator parameter to whether the player is in sight or not.
-            //anim.SetBool(hash.playerInSightBool, playerInSight);
-        //else
-            // ... set the animator parameter to false.
-            //anim.SetBool(hash.playerInSightBool, false);
     }
 
 
     void OnTriggerStay(Collider other)
     {
+        //Debug.Log("TRIGGERED");
         // If the player has entered the trigger sphere...
         if (other.gameObject == player)
         {
+            Debug.Log("TRIGGERED BY PLAYER");
             // By default the player is not in sight.
             playerInSight = false;
 
             // Create a vector from the enemy to the player and store the angle between it and forward.
             Vector3 direction = other.transform.position - transform.position;
             float angle = Vector3.Angle(direction, transform.forward);
+            //direction.y = 0;
 
             // If the angle between forward and where the player is, is less than half the angle of view...
             if (angle < fieldOfViewAngle * 0.5f)
             {
+                Debug.Log("GOOD ANGLE");
                 RaycastHit hit;
-
+                
+                //transform.position = new Vector3(transform.position.x, 1, transform.position.z);//Fix for dumb
+                Debug.Log("POSITION:" + transform.position);
+                //Debug.Log("PLYAR POS: " + player.transform.position);
+                //Debug.Log("DIRECTION:" + direction);
                 // ... and if a raycast towards the player hits something...
-                if (Physics.Raycast(transform.position + transform.up, direction.normalized, out hit, col.radius))
+                if (Physics.Raycast(transform.position + transform.up, direction, out hit, col.radius))
                 {
+                    Debug.DrawLine(transform.position, hit.point, Color.cyan, 60);
                     // ... and if the raycast hits the player...
                     if (hit.collider.gameObject == player)
                     {
+                        Debug.Log("BIG WIN");
                         // ... the player is in sight.
                         playerInSight = true;
-
                         // Set the last global sighting is the players current position.
                         personalLastSighting = player.transform.position;
                     }
                 }
             }
-
-            // Store the name hashes of the current states.
-            /*int playerLayerZeroStateHash = playerAnim.GetCurrentAnimatorStateInfo(0).nameHash;
-            int playerLayerOneStateHash = playerAnim.GetCurrentAnimatorStateInfo(1).nameHash;
-
-            // If the player is running or is attracting attention...
-            if (playerLayerZeroStateHash == hash.locomotionState || playerLayerOneStateHash == hash.shoutState)
-            {
-                // ... and if the player is within hearing range...
-                //if (CalculatePathLength(player.transform.position) <= col.radius)
-                    // ... set the last personal sighting of the player to the player's current position.
-                    personalLastSighting = player.transform.position;
-            }*/
         }
     }
 
@@ -112,39 +84,4 @@ public class EnemySight : MonoBehaviour
             // ... the player is not in sight.
             playerInSight = false;
     }
-
-
-    /*float CalculatePathLength(Vector3 targetPosition)
-    {
-        // Create a path and set it based on a target position.
-        NavMeshPath path = new NavMeshPath();
-        if (nav.enabled)
-            nav.CalculatePath(targetPosition, path);
-
-        // Create an array of points which is the length of the number of corners in the path + 2.
-        Vector3[] allWayPoints = new Vector3[path.corners.Length + 2];
-
-        // The first point is the enemy's position.
-        allWayPoints[0] = transform.position;
-
-        // The last point is the target position.
-        allWayPoints[allWayPoints.Length - 1] = targetPosition;
-
-        // The points inbetween are the corners of the path.
-        for (int i = 0; i < path.corners.Length; i++)
-        {
-            allWayPoints[i + 1] = path.corners[i];
-        }
-
-        // Create a float to store the path length that is by default 0.
-        float pathLength = 0;
-
-        // Increment the path length by an amount equal to the distance between each waypoint and the next.
-        for (int i = 0; i < allWayPoints.Length - 1; i++)
-        {
-            pathLength += Vector3.Distance(allWayPoints[i], allWayPoints[i + 1]);
-        }
-
-        return pathLength;
-    }*/
 }
