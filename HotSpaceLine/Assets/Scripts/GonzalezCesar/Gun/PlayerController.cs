2711557ff42dc92bc20ec3d3fbd6b115c291f612
Cesar Gonzalez <cmauricio86@gmail.com>
2016-03-01 19:39:00 -0800

    add player control for weapon
    hey guys I have create a script for the player that we can use for each
    different weapon I hope it works there because is not working well in
    my laptop.

---
+++ HotSpaceLine/Assets/Scripts/GonzalezCesar/Gun/PlayerController.cs
@@ -0,0 +1,93 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof (CharacterController))]
+public class PlayerController : MonoBehaviour 
+{
+
+	public float rotationSpeed = 450;
+	public float walkSpeed = 5;
+	public float runSpeed = 8;
+	private float acceleration = 5;
+	private Quaternion targetRotation;
+	private Vector3 currentVelocityMod;
+	private bool reloading;
+	public Transform hand;
+	public Weapon[] guns;
+	private Weapon currentGun;
+	private CharacterController controller;
+
+	void Start () 
+	{
+		controller = GetComponent<CharacterController>();
+		EquipGun(0);
+	}
+
+	void Update () 
+	{
+		ControlMouse();
+		if (currentGun) {
+			if (Input.GetButtonDown("Shoot")) 
+			{
+				currentGun.Shoot();
+			}
+			else if (Input.GetButton("Shoot")) 
+			{
+				currentGun.ShootContinuous();
+			}
+
+			if (Input.GetButtonDown("Reload")) 
+			{
+				if (currentGun.Reload()) 
+				{
+					reloading = true;
+				}
+			}
+
+			if (reloading) 
+			{ 
+					currentGun.FinishReload();
+					reloading = false;
+			}
+		}
+
+		for (int i = 0; i < guns.Length; i++) 
+		{
+			if (Input.GetKeyDown((i+1) + "") || Input.GetKeyDown("[" + (i+1) + "]")) 
+			{
+				EquipGun(i);
+				break;
+			}
+		}	
+	}
+
+	void EquipGun(int i) 
+	{
+		if (currentGun) 
+		{
+			Destroy(currentGun.gameObject);
+		}
+
+		currentGun = Instantiate(guns[i],hand.position,hand.rotation) as Weapon;
+		currentGun.transform.parent = hand;
+	}
+
+	void ControlMouse() 
+	{
+
+		Vector3 mousePos = Input.mousePosition;
+		targetRotation = Quaternion.LookRotation(mousePos - new Vector3(transform.position.x,0,transform.position.z));
+		transform.eulerAngles = Vector3.up * Mathf.MoveTowardsAngle(transform.eulerAngles.y,targetRotation.eulerAngles.y,rotationSpeed * Time.deltaTime);
+
+		Vector3 input = new Vector3(Input.GetAxisRaw("Horizontal"),0,Input.GetAxisRaw("Vertical"));
+
+		currentVelocityMod = Vector3.MoveTowards(currentVelocityMod,input,acceleration * Time.deltaTime);
+		Vector3 motion = currentVelocityMod;
+		motion *= (Mathf.Abs(input.x) == 1 && Mathf.Abs(input.z) == 1)?.7f:1;
+		motion *= (Input.GetButton("Run"))?runSpeed:walkSpeed;
+		motion += Vector3.up * -8;
+		
+		controller.Move(motion * Time.deltaTime);
+	}
+
+}
--- HotSpaceLine/Assets/Scripts/GonzalezCesar/Gun/Weapon.cs
+++ HotSpaceLine/Assets/Scripts/GonzalezCesar/Gun/Weapon.cs
@@ -4,215 +4,30 @@ using System.Collections;
 [RequireComponent (typeof (AudioSource))]
 public class Weapon : MonoBehaviour 
 {
-    public enum GunType { Semi, Auto, Shotgun };
-    public LayerMask collisionMask;
-    public float gunID;
-    public GunType gunType;
-    public float rpm;
-    public float damage = 1;
-    public int totalAmmo = 40;
-    public int ammoPerMag = 10;
-    public Transform spawn;
-    public Transform shellEjectionPoint;
-    public Rigidbody shell;
-    private LineRenderer tracer;
-    public int bulletForce = 500;
-    private float secondsBetweenShots;
-    private float nextPossibleShootTime;
-    private int currentAmmoInMag;
-    private bool reloading;
-
-    void Start()
-    {
-        secondsBetweenShots = 60 / rpm;
-        if (GetComponent<LineRenderer>())
-        {
-            tracer = GetComponent<LineRenderer>();
-        }
-
-        currentAmmoInMag = ammoPerMag;
-
-    }
-
-    public void Shoot()
-    {
-
-        if (CanShoot())
-        {
-            Debug.Log("hit");
-            Ray ray = new Ray(spawn.position, spawn.forward);
-            RaycastHit hit;
-
-            float shotDistance = 20;
-
-            if (Physics.Raycast(ray, out hit, shotDistance, collisionMask))
-            {
-                shotDistance = hit.distance;
-
-                //if (hit.collider.GetComponent<EnemyHealth>())
-                //{
-                //    hit.collider.GetComponent<EnemyHealth>().TakeDamage(damage);
-                //}
-            }
-
-            nextPossibleShootTime = Time.time + secondsBetweenShots;
-            currentAmmoInMag--;
-
-            GetComponent<AudioSource>().Play();
-
-            if (tracer)
-            {
-                StartCoroutine("RenderTracer", ray.direction * shotDistance);
-            }
-
-            Rigidbody newShell = Instantiate(shell, shellEjectionPoint.position, Quaternion.identity) as Rigidbody;
-            newShell.AddForce(bulletForce*(shellEjectionPoint.forward + spawn.forward ));
-            Debug.Log(Random.Range(150f, 200f));
-
-
-        }
-
-    }
-
-    public void ShootContinuous()
-    {
-        if (gunType == GunType.Auto)
-        {
-            Shoot();
-        }
-    }
-
-    public void Spread()
-    {
-        if (gunType == GunType.Shotgun)
-        {
-            Shoot();        
-        }
-    }
-
-    //public void Spread()
-    //{
-    //    if (gunType == GunType.Shotgun)
-        
-    //         Vector3 Spread ( Vector3 aim ,   float distance ,   float variance  )
-    // aim.Normalize();
-    // Vector3 v3;
-    // do 
-    //     v3 = Random.insideUnitSphere;
-    // while (v3 == aim || v3 == -aim);
-    // v3 = Vector3.Cross(aim, v3);
-    // v3 = v3 * Random.Range(0.0f, variance);
-    // return aim * distance + v3;
- 
-        
-    //}
-    
-         
-
-    private bool CanShoot()
-    {
-        bool canShoot = true;
-
-        Debug.Log("can't shoot");
-        if (Time.time < nextPossibleShootTime)
-        {
-            Debug.Log("waiting");
-            canShoot = false;
-        }
-
-        if (currentAmmoInMag == 0)
-        {
-            Debug.Log("No ammo");
-            canShoot = false;
-        }
-
-        if (reloading)
-        {
-            Debug.Log("reloading");
-            canShoot = false;
-        }
-
-
-        return canShoot;
-    }
-
-    void Update()
-    {
-        if (Input.GetButtonDown("Shoot"))
-        {
-            Shoot();
-        }
-        else if (Input.GetButton("Shoot"))
-        {
-            ShootContinuous();
-        }
-    }
-
-    public bool Reload()
-    {
-        if (totalAmmo != 0 && currentAmmoInMag != ammoPerMag)
-        {
-            reloading = true;
-            return true;
-        }
-
-        return false;
-    }
-
-    public void FinishReload()
-    {
-        reloading = false;
-        currentAmmoInMag = ammoPerMag;
-        totalAmmo -= ammoPerMag;
-
-        if (totalAmmo < 0)
-        {
-            currentAmmoInMag += totalAmmo;
-            totalAmmo = 0;
-        }
-
-    }
-
-    IEnumerator RenderTracer(Vector3 hitPoint)
-    {
-        //tracer.enabled = true;
-        tracer.SetPosition(0, spawn.position);
-        tracer.SetPosition(1, spawn.position + hitPoint);
-
-        yield return null;
-        tracer.enabled = false;
-    }
-
-    /*
-	public enum GunType {Auto,Semi,Burst, Shutgun};
+	public enum GunType {Semi,Burst,Auto};
+	//public float gunID;
 	public GunType gunType;
 	public float rpm;
+	public int totalAmmo = 40;
+	public int ammoPerMag = 10;
 	public Transform spawn;
-	public Transform bulletEjectionPoint;
-	public Rigidbody bullet;
-    public Weapon weapon;
+	public Transform shellEjectionPoint;
+	public Rigidbody shell;
 	private LineRenderer tracer;
 	private float secondsBetweenShots;
 	private float nextPossibleShootTime;
+	private int currentAmmoInMag;
+	private bool reloading;
 	
 	void Start() {
 		secondsBetweenShots = 60/rpm;
 		if (GetComponent<LineRenderer>()) {
 			tracer = GetComponent<LineRenderer>();
 		}
-	}
+		
+		currentAmmoInMag = ammoPerMag;
 
-    void Update()
-    {
-        if (Input.GetButtonDown("Shoot"))
-        {
-            Shoot();
-        }
-        else if (Input.GetButton("Shoot"))
-        {
-            ShootContinuous();
-        }
-    }
+	}
 	
 	public void Shoot() {
 		
@@ -227,6 +42,7 @@ public class Weapon : MonoBehaviour
 			}
 			
 			nextPossibleShootTime = Time.time + secondsBetweenShots;
+			currentAmmoInMag --;
 			
 			GetComponent<AudioSource>().Play();
 			
@@ -234,8 +50,8 @@ public class Weapon : MonoBehaviour
 				StartCoroutine("RenderTracer", ray.direction * shotDistance);
 			}
 			
-			Rigidbody newBullet = Instantiate(bullet,bulletEjectionPoint.position,Quaternion.identity) as Rigidbody;
-			newBullet.AddForce(bulletEjectionPoint.forward * Random.Range(150f,200f) + spawn.forward * Random.Range(-10f,10f));
+			Rigidbody newShell = Instantiate(shell,shellEjectionPoint.position,Quaternion.identity) as Rigidbody;
+			newShell.AddForce(shellEjectionPoint.forward * Random.Range(150f,200f) + spawn.forward * Random.Range(-10f,10f));
 		}
 		
 	}
@@ -253,7 +69,36 @@ public class Weapon : MonoBehaviour
 			canShoot = false;
 		}
 		
+		if (currentAmmoInMag == 0) {
+			canShoot = false;
+		}
+		
+		if (reloading) {
+			canShoot = false;
+		}
+		
+		
 		return canShoot;
+	}
+	
+	public bool Reload() {
+		if (totalAmmo != 0 && currentAmmoInMag != ammoPerMag) {
+			reloading = true;
+			return true;
+		}
+		
+		return false;
+	}
+	
+	public void FinishReload() {
+		reloading = false;
+		currentAmmoInMag = ammoPerMag;
+		totalAmmo -= ammoPerMag;
+		
+		if (totalAmmo < 0) {
+			currentAmmoInMag += totalAmmo;
+			totalAmmo = 0;
+		}
 	}
 	
 	IEnumerator RenderTracer(Vector3 hitPoint) {
@@ -263,6 +108,6 @@ public class Weapon : MonoBehaviour
 		
 		yield return null;
 		tracer.enabled = false;
-	}*/
+	}
 }
 
